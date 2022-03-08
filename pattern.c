83be461c95305482a29f1246474da62738fc1ce0
SACHIT260 <96931281+SACHIT260@users.noreply.github.com>
2022-03-08 17:15:22 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+void main()
+{
+    int n,i,j;
+    printf("Enter a number-");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+        if(i==1 || i==n)
+        {
+            for(j=1;j<=n;j++)
+                printf("* ");
+            printf("\n");
+        }
+        else
+        {
+            for(j=1;j<=n;j++)
+            {
+                if(j==1 || j==n)
+                    printf("* ");
+                else
+                    printf("  ");
+            }
+            printf("\n");
+        }
+}
